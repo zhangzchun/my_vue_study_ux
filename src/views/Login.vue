fde5eb5981bb26068df3bdde48a8454d2def1322
zhangzchwn <1064494254@qq.com>
2020-08-04 07:55:02 +0800

    mapState

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,74 +6,79 @@
         <button @click="logout" v-else>注销</button>
     </div>
 </template>
-<script> export default {
-    methods: {
-        login() {
-            // window.isLogin = true;
-            // 修改状态只能通过store.dispatch(mutation)
-            // 提交 mutation 修改状态
-            // this.$store.commit('login')
-            // 派发动作 触发action store.dispatch
-            this.$store.dispatch('user/login', 'admin').then(() => {
+<script>
+    import {mapState} from 'vuex'
 
-            // 动态添加路由
-            this.$router.addRoutes([
-                {
-                    path: '/admin',
-                    name: 'admin',
-                    // route level code-splitting
-                    // this generates a separate chunk (about.[hash].js) for this route
-                    // which is lazy-loaded when the route is visited.
-                    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-                    component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue'),
-                    children: [
+    export default {
+        methods: {
+            login() {
+                // window.isLogin = true;
+                // 修改状态只能通过store.dispatch(mutation)
+                // 提交 mutation 修改状态
+                // this.$store.commit('login')
+                // 派发动作 触发action store.dispatch
+                this.$store.dispatch('user/login', 'admin').then(() => {
+
+                    // 动态添加路由
+                    this.$router.addRoutes([
                         {
-                            // path: '/course/:name',
-                            path: '/admin/course/:name',
-                            name: 'detail',
-                            component: () => import('../views/Detail.vue')
+                            path: '/admin',
+                            name: 'admin',
+                            // route level code-splitting
+                            // this generates a separate chunk (about.[hash].js) for this route
+                            // which is lazy-loaded when the route is visited.
+                            // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+                            component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue'),
+                            children: [
+                                {
+                                    // path: '/course/:name',
+                                    path: '/admin/course/:name',
+                                    name: 'detail',
+                                    component: () => import('../views/Detail.vue')
+                                },
+                            ],
+                            meta: {
+                                auth: true,
+                            },
+                            // beforeEnter(to, from, next) {
+                            //     /*if (to.meta.auth) {
+                            //         if (window.isLogin) {
+                            //             next()
+                            //         } else {
+                            //             next('/login?redirect=' + to.fullPath)
+                            //         }
+                            //     } else {
+                            //         next()
+                            //     }*/
+                            //     if (window.isLogin) {
+                            //         next()
+                            //     } else {
+                            //         next('/login?redirect=' + to.fullPath)
+                            //     }
+                            // }
                         },
-                    ],
-                    meta: {
-                        auth: true,
-                    },
-                    // beforeEnter(to, from, next) {
-                    //     /*if (to.meta.auth) {
-                    //         if (window.isLogin) {
-                    //             next()
-                    //         } else {
-                    //             next('/login?redirect=' + to.fullPath)
-                    //         }
-                    //     } else {
-                    //         next()
-                    //     }*/
-                    //     if (window.isLogin) {
-                    //         next()
-                    //     } else {
-                    //         next('/login?redirect=' + to.fullPath)
-                    //     }
-                    // }
-                },
-            ]);
-            this.$router.push(this.$route.query.redirect)
-            }).catch(() => {
-                alert('用户名或密码错误')
-            });
+                    ]);
+                    this.$router.push(this.$route.query.redirect)
+                }).catch(() => {
+                    alert('用户名或密码错误')
+                });
+            },
+            logout() {
+                // window.isLogin = false
+                // this.$store.commit('logout')
+                this.$store.commit('user/logout')
+                this.$router.push('/')
+            }
+        }, computed: {
+            /*isLogin() {
+                // return window.isLogin
+                // return this.$store.state.isLogin
+                return this.$store.state.user.isLogin
+            }*/
+            ...mapState('user', ['isLogin'])
         },
-        logout() {
-            // window.isLogin = false
-            // this.$store.commit('logout')
-            this.$store.commit('user/logout')
-            this.$router.push('/')
-        }
-    }, computed: {
-        isLogin() {
-            // return window.isLogin
-            // return this.$store.state.isLogin
-            return this.$store.state.user.isLogin
-        }
-    },
-} </script>
+    }
+</script>
 <style scoped>
 
 </style>
